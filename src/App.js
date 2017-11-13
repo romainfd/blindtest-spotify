c8698855ffd6b84a00527baf03ae0462f02e4db3
Romain Fouilland <romain.fouilland@polytechnique.edu>
2017-11-13 19:05:15 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -42,7 +42,8 @@ class App extends Component {
           <h1 className="App-title">Bienvenue sur le Blindtest</h1>
         </header>
         <div className="App-images">
-          <p>Il va falloir modifier le code pour faire un vrai Blindtest !</p>
+          <p>Je mets du texte en plus !
+          Il va falloir modifier le code pour faire un vrai Blindtest !</p>
         </div>
         <div className="App-buttons">
         </div>
